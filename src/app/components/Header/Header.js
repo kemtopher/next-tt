6f66cdb277738d0bc25b3a981648135dccbec20a
Chris Kemeza <ckemeza1@gmail.com>
2025-07-03 18:31:48 -0400

    header alignment

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { MainLogo } from '../MainLogo/MainLogo'
+import { SocialBar } from '../social-bar/SocialBar'
 
 export const Header = () => {
   return (
-    <header className="h-52 w-full grid-cols-12">
-        <div className="flex justify-between">
-            <div className="col-span-2 pt-7">
+    <header className="h-52 w-full grid-cols-12 border-b-1 border-black">
+        <div className="w-full flex justify-between">
+            <div className="col-span-2 pt-7 self-center">
                 <MainLogo />
             </div>
 
-            <nav className="col-span-6 col-start-7 pt-14">
-              <ul className="hidden md:flex justify-end space-x-6">
-                <a href="#" className="text-sm font-display hover:text-accent font-medium">Lessons</a>
-                <a href="#" className="text-sm font-display hover:text-accent font-medium">Journal</a>
-                <a href="#" className="text-sm font-display hover:text-accent font-medium">Back Page</a>
-                <a href="#" className="text-sm font-display hover:text-accent font-medium">Calendar</a>
-                <a href="#" className="text-sm font-display hover:text-accent font-medium">Contact</a>
-              </ul>
-            </nav>
+            <div className="hidden md:flex flex-col col-span-6 col-start-7 pt-14 self-start">
+              <nav>
+                <ul className="hidden md:flex justify-end space-x-6">
+                  <a href="#" className="text-sm font-display hover:text-accent font-medium">Lessons</a>
+                  <a href="#" className="text-sm font-display hover:text-accent font-medium">Journal</a>
+                  <a href="#" className="text-sm font-display hover:text-accent font-medium">Back Page</a>
+                  <a href="#" className="text-sm font-display hover:text-accent font-medium">Calendar</a>
+                  <a href="#" className="text-sm font-display hover:text-accent font-medium">Contact</a>
+                </ul>
+              </nav>
+              <div className="flex justify-end mt-8">
+                <SocialBar />
+              </div>
+            </div>
         </div>
     </header>
   )
--- src/app/components/social-bar/SocialBar.js
+++ src/app/components/social-bar/SocialBar.js
@@ -5,9 +5,9 @@ import IconOnlyfans from '../icons/IconOnlyfans'
 import IconBluesky from '../icons/IconBluesky'
 import IconYoutube from '../icons/IconYoutube'
 
-export const SocialBar = () => {
+export const SocialBar = ({ width= "w-1/2" }) => {
   return (
-    <div className="flex gap-y-3.5 justify-between">
+    <div className={`${width} flex justify-between gap-2`}>
         <IconInstagram />
         <IconOnlyfans />
         <IconBluesky />
