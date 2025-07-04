e7cca1738c73ad4ef80b574e94310a336bdd3de7
Chris Kemeza <ckemeza1@gmail.com>
2025-07-04 00:30:12 -0400

    page title with responsive sizing

---
+++ src/app/components/PageHeader/PageHeader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PageHeader = ({ title }) => {
+  return (
+    <h1 className="w-full pt-3 md:pt-4 lg:pt-6 pb-4 md:pb-6 lg:pb-8 font-display uppercase text-3xl md:text-6xl lg:text-9xl border-b-1 border-black flex align-center">{title}</h1>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,14 @@
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { GridContainer } from "./components/GridContainer/GridContainer";
+import { PageHeader } from "./components/PageHeader/PageHeader";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="w-full">
-        <GridContainer>
-          <h1 className="w-full col-span-6 md:col-span-12 text-center">grid container</h1>
-        </GridContainer>
+        <PageHeader title="Journal" />
       </main>
       <Footer />
     </>    
