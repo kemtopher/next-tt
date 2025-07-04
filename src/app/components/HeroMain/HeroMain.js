8265037254c407a22a14b99c83c812cd059abaa4
Chris Kemeza <ckemeza1@gmail.com>
2025-07-04 02:35:59 -0400

    hero responsive styles

--- src/app/components/HeroMain/HeroMain.js
+++ src/app/components/HeroMain/HeroMain.js
@@ -3,15 +3,17 @@ import { GridContainer } from '../GridContainer/GridContainer'
 
 export const HeroMain = () => {
   return (
-    <div className="w-full h-[581px]">
+    <section className="w-full h-[581px] pb-15 md:pb-26">
         <GridContainer>
-            <div className="col-span-6">
-                <div className="h-full flex flex-col gap-8 items-end">
-                    <h2 className="font-display font-bold text-8xl">TT Mahony</h2>
-                    <h3 className="font-display text-5xl/15">The Toppermost of the Poppermost</h3>
+            <div className="col-span-4 md:col-span-6">
+                <div className="h-full flex flex-col items-start md:items-center justify-end">
+                  <div className="compo flex flex-col max-w-xs md:max-w-full gap-4 md:gap-8">
+                    <h2 className="font-display font-bold text-5xl md:text-7xl lg:text-8xl">TT Mahony</h2>
+                    <h3 className="font-display text-2xl md:text-4xl/12 lg:text-5xl/15">The Toppermost of the Poppermost</h3>
+                </div>
                 </div>
             </div>                
         </GridContainer>
-    </div>
+    </section>
   )
 }
