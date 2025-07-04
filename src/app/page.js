c7ebc16f8e4bfd2616b5dcb9b074105d5d258565
Chris Kemeza <ckemeza1@gmail.com>
2025-07-04 02:13:03 -0400

    subheader section responsive setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { PageHeader } from "./components/PageHeader/PageHeader";
+import { SubHeader } from "./components/SubHeader/SubHeader";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <PageHeader title="Journal" />
+      <SubHeader content="Pianist, Teacher, Raconteur, Poncey Lad"/>
       <main className="w-full pt-8 md:pt-12 lg:pt-18">
 
       </main>
