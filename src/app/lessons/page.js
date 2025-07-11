90da857cba0b92b63fe567b93a0a2e2525ad771e
Chris Kemeza <ckemeza1@gmail.com>
2025-07-10 20:31:10 -0400

    base quote component plus svg icons for refactor

--- src/app/lessons/page.js
+++ src/app/lessons/page.js
@@ -6,14 +6,20 @@ import { Footer } from '../../components/Footer/Footer'
 import { PageIntro } from '../../components/PageIntro/PageIntro'
 import { ImageWithText } from '../../components/ImageWithText/ImageWithText'
 import Image from 'next/image'
+import { QuoteBlock } from '../../components/QuoteBlock/QuoteBlock'
 
 export default function Lessons() {
+    const quote = `He who who binds to himself a joy\n\nDoes the winged life destroy;\n\nHe who kisses the joy as it flies\n\nLives in eternity’s sunrise.`;
+
+    const quoteRich = quote.split('\n').map((line, i) => (
+                        <p key={i}>{line}</p>
+                    ))
   return (
     <>
         <Header />
         <main className="w-full pb-8 md:pb-12 lg:pb-18">
             <PageHeader title="LESSONS" />
-            <GridContainer classes="py-16 flex flex-col">
+            <GridContainer classes="pt-16 flex flex-col">
                 <PageIntro content="T.T. has been teaching in various contexts since 2002. He has guided 3-year-olds in beginning Suzuki lessons, prepared students for competitions and college auditions, and many way-stations in between. The student's goals and personal strengths and weaknesses guide his approach."/>
                 
                 <ImageWithText
@@ -21,6 +27,11 @@ export default function Lessons() {
                     imageUrl='/lessons-piano.jpg'
                     alt="Encouraging image of kite flying in the wind"
                     content="Students are grounded in music theory, reading and ear training, and a fluent and effortless technique is cultivated from the first lesson. As with anything, we get out of music what we put into it, and T.T. teaches that a truly focused and intentional five minutes spent can change everything."
+                />
+                <QuoteBlock
+                    content={quoteRich}
+
+                    author="William Blake"
                 />
             </GridContainer>
         </main>
---
+++ src/components/QuoteBlock/QuoteBlock.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const QuoteBlock = ({ content, author }) => {
+  return (
+    <section className="pt-16 text-center col-span-6 md:col-span-12">
+      <div className="py-12 px-10 font-display font-bold flex flex-col gap-8 relative">
+        {/* <div className="text-[250px] absolute -top-1/4">“</div> */}
+        <div className="text-xl md:text-3xl text-black whitespace-pre-line">{content}</div>
+        {/* <div className="text-[250px] absolute">”</div> */}
+        <p className="mt-8 text-sm tracking-wide font-medium uppercase">- {author}</p>
+      </div>
+    </section>
+  );
+};
