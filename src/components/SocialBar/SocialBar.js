8309cdf27fa76b314e49f39d9478f0405d1eb8cc
Chris Kemeza <ckemeza1@gmail.com>
2025-07-29 14:06:18 -0400

    increase margin for visual space

--- src/components/SocialBar/SocialBar.js
+++ src/components/SocialBar/SocialBar.js
@@ -8,7 +8,7 @@ import Link from 'next/link';
 
 export const SocialBar = ({ width = 'min-w-[100px] md:min-w-[185px]' }) => {
   return (
-    <div className={`${width} flex justify-between gap-2 mt-4`}>
+    <div className={`${width} flex justify-between gap-2 mt-6`}>
       <Link href="">
         <IconInstagram classes="w-6 h-6 sm:w-8 sm:h-8" />
       </Link>
