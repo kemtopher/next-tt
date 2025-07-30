f73fa9f11c7e5425c0e2812a2252aacd1767e9ed
Chris Kemeza <ckemeza1@gmail.com>
2025-07-29 23:57:32 -0400

    quote section responsiveness

--- src/components/QuoteBlock/QuoteBlock.js
+++ src/components/QuoteBlock/QuoteBlock.js
@@ -3,11 +3,11 @@ import React from 'react';
 export const QuoteBlock = ({ content, author }) => {
   return (
     <section className="pt-10 text-center col-span-6 md:col-span-12 border-t-1">
-      <div className="py-12 px-10 font-display font-bold flex flex-col gap-8 relative">
-        <div className="text-xl md:text-3xl text-black whitespace-pre-line">
+      <div className="py-12 px-8 md:px-10 font-display font-bold flex flex-col gap-8 relative">
+        <div className="text-md sm:text-xl md:text-3xl text-black whitespace-pre-line">
           {content}
         </div>
-        <p className="mt-8 text-sm tracking-wide font-medium uppercase">
+        <p className="mt-0 md:mt-8 text-sm md:text-md tracking-wide font-medium uppercase">
           - {author}
         </p>
       </div>
