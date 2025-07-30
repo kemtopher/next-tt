50f11fcf8617632988d2e47df3a262bb7d7f4dba
Chris Kemeza <ckemeza1@gmail.com>
2025-07-30 16:40:46 -0400

    header width bug fixed

--- src/components/AnimatedHeader/AnimatedHeader.js
+++ src/components/AnimatedHeader/AnimatedHeader.js
@@ -49,57 +49,59 @@ export const AnimatedHeader = ({heroRef}) => {
 
   return (
     <header ref={headerRef} className={styles.header}>
-      <div className="w-full py-7 flex justify-between items-center">
-        {/* <div className="self-center w-[92px] h-[106px] relative"> */}
-          <motion.div
-            className="self-center w-[92px] h-[106px] relative origin-top-left"
-            animate={logoControls}
-            initial={{ scale: initialScale }}
-            transition={{ type: 'tween', ease: 'linear', duration: 0 }}
-          >
-            <Link href="/">
-              <MainLogo classes={styles.scaleLogo} />
-            </Link>
-          </motion.div>
-        {/* </div> */}
+      <div className={styles.headerWrapper}>
+        <div className="w-full py-7 flex justify-between items-center">
+          {/* <div className="self-center w-[92px] h-[106px] relative"> */}
+            <motion.div
+              className="self-center w-[92px] h-[106px] relative origin-top-left"
+              animate={logoControls}
+              initial={{ scale: initialScale }}
+              transition={{ type: 'tween', ease: 'linear', duration: 0 }}
+            >
+              <Link href="/">
+                <MainLogo classes={styles.scaleLogo} />
+              </Link>
+            </motion.div>
+          {/* </div> */}
 
-        <div className="h-2/3 hidden md:flex flex-col justify-between self-center">
-          <nav>
-            <ul className="hidden md:flex justify-end space-x-6">
-              <Link
-                href="/lessons"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Lessons
-              </Link>
-              <Link
-                href="/journal"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Journal
-              </Link>
-              <Link
-                href="/backpages"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Back Page
-              </Link>
-              <Link
-                href="/calendar"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Calendar
-              </Link>
-              <Link
-                href="/contact"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Contact
-              </Link>
-            </ul>
-          </nav>
-          <div className="flex justify-end">
-            <SocialBar />
+          <div className="h-2/3 hidden md:flex flex-col justify-between self-center">
+            <nav>
+              <ul className="hidden md:flex justify-end space-x-6">
+                <Link
+                  href="/lessons"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Lessons
+                </Link>
+                <Link
+                  href="/journal"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Journal
+                </Link>
+                <Link
+                  href="/backpages"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Back Page
+                </Link>
+                <Link
+                  href="/calendar"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Calendar
+                </Link>
+                <Link
+                  href="/contact"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Contact
+                </Link>
+              </ul>
+            </nav>
+            <div className="flex justify-end">
+              <SocialBar />
+            </div>
           </div>
         </div>
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,50 +10,52 @@ export const Header = () => {
 
   return (
     <header className={styles.header}>
-      <div className="w-full py-7 flex justify-between items-center">
-        <div className="self-center w-[92px] h-[106px] relative">
-            <Link href="/">
-              <MainLogo classes={styles.scaleLogo} />
-            </Link>
-        </div>
+      <div className={styles.headerWrapper}>
+        <div className="w-full py-7 flex justify-between items-center">
+          <div className="self-center w-[92px] h-[106px] relative">
+              <Link href="/">
+                <MainLogo classes={styles.scaleLogo} />
+              </Link>
+          </div>
 
-        <div className="h-2/3 hidden md:flex flex-col justify-between self-center">
-          <nav>
-            <ul className="hidden md:flex justify-end space-x-6">
-              <Link
-                href="/lessons"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Lessons
-              </Link>
-              <Link
-                href="/journal"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Journal
-              </Link>
-              <Link
-                href="/backpages"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Back Page
-              </Link>
-              <Link
-                href="/calendar"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Calendar
-              </Link>
-              <Link
-                href="/contact"
-                className="text-sm font-display hover:text-accent font-medium"
-              >
-                Contact
-              </Link>
-            </ul>
-          </nav>
-          <div className="flex justify-end">
-            <SocialBar />
+          <div className="h-2/3 hidden md:flex flex-col justify-between self-center">
+            <nav>
+              <ul className="hidden md:flex justify-end space-x-6">
+                <Link
+                  href="/lessons"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Lessons
+                </Link>
+                <Link
+                  href="/journal"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Journal
+                </Link>
+                <Link
+                  href="/backpages"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Back Page
+                </Link>
+                <Link
+                  href="/calendar"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Calendar
+                </Link>
+                <Link
+                  href="/contact"
+                  className="text-sm font-display hover:text-accent font-medium"
+                >
+                  Contact
+                </Link>
+              </ul>
+            </nav>
+            <div className="flex justify-end">
+              <SocialBar />
+            </div>
           </div>
         </div>
       </div>
