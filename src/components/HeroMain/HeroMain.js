06cf26b69792170bc8255c84cef2548716bc57d5
Chris Kemeza <ckemeza1@gmail.com>
2025-08-04 17:52:17 -0400

    hero mobile bug debugging

--- src/components/HeroMain/HeroMain.js
+++ src/components/HeroMain/HeroMain.js
@@ -6,7 +6,7 @@ import { GridContainer } from '../GridContainer/GridContainer';
 
 export const HeroMain = ({ header, subheader, heroRef }) => {
   return (
-    <section ref={heroRef} className="w-full h-[581px] pt-8 md:pt-12 lg:pt-18 pb-15 md:pb-26">
+    <section ref={heroRef} className="w-full h-[581px] pt-8 md:pt-12 lg:pt-18 pb-20 md:pb-26">
       <GridContainer>
         <div className="md:col-start-8 col-span-4 md:col-span-6">
           <div className="h-full flex flex-col justify-end">
