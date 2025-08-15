8d560ccbe75152421075ffd2a3ba1c88137edd66
Chris Kemeza <ckemeza1@gmail.com>
2025-08-15 02:19:08 -0400

    reverted to Countdown component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import { createClient } from '../prismicio';
-import HomeContent from '../components/HomeContent/HomeContent';
-// import Countdown from '../components/Countdown/Countdown';
+// import HomeContent from '../components/HomeContent/HomeContent';
+import Countdown from '../components/Countdown/Countdown';
 
 export default async function Home() {
   const client = createClient();
   const homePage = await client.getSingle('home_page');
 
-  return <HomeContent homePage={homePage} />;
-  // return <Countdown />
+  // return <HomeContent homePage={homePage} />;
+  return <Countdown />
 }
