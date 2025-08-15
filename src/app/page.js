4b7de53298b99dcab37564e0949b84e60a61b3d7
Chris Kemeza <ckemeza1@gmail.com>
2025-08-14 22:18:27 -0400

    working journal template

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import { createClient } from '../prismicio';
-import HomeContent from '../components/HomeContent/HomeContent';
+// import HomeContent from '../components/HomeContent/HomeContent';
+import Countdown from '../components/Countdown/Countdown';
 
 export default async function Home() {
   const client = createClient();
   const homePage = await client.getSingle('home_page');
 
-  return <HomeContent homePage={homePage} />;
+  // return <HomeContent homePage={homePage} />;
+  return <Countdown />
 }
