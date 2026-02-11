ff35b5e28b7cd429ad5d37e8b0b5aa3a4b1a61bd
Chris Kemeza <ckemeza1@gmail.com>
2026-02-11 12:32:55 -0500

    build: eslint rules and script added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 
+
+import unusedImports from 'eslint-plugin-unused-imports';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,6 +13,25 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals')];
-
-export default eslintConfig;
+export default [
+    ...compat.extends('next/core-web-vitals'),
+ 
+    {
+      plugins: {
+        'unused-imports': unusedImports,
+        'simple-import-sort': simpleImportSort,
+      },
+      rules: {
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': ['warn', { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }],
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'prefer-const': 'error',
+        'no-console': 
+          process.env.NODE_ENV === 'production'
+            ? 'error' 
+            : 'warn',
+        'consistent-return': 'warn',
+      },
+    }
+]
