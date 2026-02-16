a7ea1a65957f553037bc04c6963a660e87b6ad47
Chris Kemeza <ckemeza1@gmail.com>
2026-02-16 16:55:59 -0500

    lint: changed img to us next/image on backpage card bg

--- src/app/backpages/page.js
+++ src/app/backpages/page.js
@@ -1,4 +1,5 @@
 import { asDate, asText } from '@prismicio/client';
+import Image from 'next/image';
 import React from 'react';
 
 import { BackPageCard } from '../../components/BackPageCard/BackPageCard';
@@ -35,10 +36,12 @@ export default async function BackPages() {
                             className="mb-6 break-inside-avoid overflow-hidden relative"
                         >
                             <div className="w-full h-full absolute top-0 left-0">
-                                <img
+                                <Image
                                     src={entry.data.bg_img.url}
                                     className="w-full h-full object-cover object-top"
                                     alt="Article card with the text above it"
+                                    width={entry.data.bg_img.width}
+                                    height={entry.data.bg_img.height}
                                 />
                             </div>
                             <BackPageCard
